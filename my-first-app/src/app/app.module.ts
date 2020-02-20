cc1e720e753e9bbf49b87ffee80b1b0f5c3e0b9b
Lane Brian (BGV7PPY) <brianlane@ups.com>
2020-02-20 15:17:57 -0500

    Added first app and first basics assignment

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
