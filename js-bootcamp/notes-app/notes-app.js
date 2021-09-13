c4b8535d6f21ff7761f69ac3728e3011b47561f2
Brian L <bplane92@gmail.com>
2021-09-12 22:36:15 -0400

    Started on section 7: JS in the Browser

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -1,3 +1,4 @@
+// DOM - Document Object Model
 const notes = [{
     title: 'My next trip',
     body: 'I would like to go to space'
@@ -9,22 +10,86 @@ const notes = [{
     body: 'Get a new chair'
 }]
 
-// DOM - Document Object Model
+// Filter object
+const filters = {
+    searchText: ''
+}
+
+// Filter note list
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    // Clear div id='notes'
+    document.querySelector('#notes').innerHTML = ''
+
+    // When filtering notes, only display ones that matched characters 
+    filteredNotes.forEach(function(note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+// e = event
+// Targeting by tag name
+// document.querySelector('button').addEventListener('click', function(e) {
+//     e.target.textContent = 'The button was clicked'
+// })
+
+// Targeting by id
+document.querySelector('#create-note').addEventListener('click', function(e) {
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function() {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove()
+    })
+})
+
+// Targeting an input field 
+// on 'change' will output when focus is off of field
+// on 'input' will output all characters as they're typed
+// Call renderNotes function to show matching notes on the page
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+// -- Single --
+// p
+// #replace
+// .item
+
+// -- Multiple -- 
+// p#order
+// button.inventory
+// h1#title.application  targeting h1 element, with id of title and class of application
+// h1.application#title  targeting h1 element, with class application and id of title
+
+// Targeting by button type and chosing second one
+// document.querySelectorAll('button')[1].addEventListener('click', function() {
+//     console.log('Delete all notes')
+// })
 
 // Query and remove
 // const p = document.querySelector('p')
 // p.remove()
 
 // Query all and remove
-const ps = document.querySelectorAll('p')
+// const ps = document.querySelectorAll('p')
 
-ps.forEach(function(p) {
-    p.textContent = '*******'
-        // console.log(p.textContent)
-        // p.remove()
-})
+// ps.forEach(function(p) {
+//     p.textContent = '*******'
+//         console.log(p.textContent)
+//         p.remove()
+// })
 
 // add a new element
-const newParagraph = document.createElement('p')
-newParagraph.textContent = 'This is a new element from JavaScript!'
-document.querySelector('body').appendChild(newParagraph)
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JavaScript!'
+// document.querySelector('body').appendChild(newParagraph)
--- js-bootcamp/todo-app/todo-app.js
+++ js-bootcamp/todo-app/todo-app.js
@@ -1,4 +1,4 @@
-const toDo = [{
+const todos = [{
     text: 'Complete this section',
     complete: false
 }, {
@@ -15,25 +15,54 @@ const toDo = [{
     complete: false
 }]
 
-// testing
+// 1. Setup a div contain for todos - Done
+// 2. Setup filters (searchText) and wire up a new filter input to change it
+const filters = {
+        searchText: ''
+    }
+    // 3. Create a renderTodos function to render and rerender the latest filtered data
+const renderTodos = function(todos, filter) {
+    const filteredToDos = todos.filter(function(todo) {
+            return todo.text.toLowerCase().includes(filters.searchText.toLocaleLowerCase())
+        })
+        // Return only the items that have not been completed
+    const getThingsToDo = filteredToDos.filter(function(todo) {
+        return !todo.complete
+    })
 
-// Return only the items that have not been completed
-const getThingsToDo = toDo.filter(function(todo) {
-    return !todo.complete
+    document.querySelector('#todos').innerHTML = ''
+
+    // You have 2 todos left (p element)
+    const todosLeft = document.createElement('h2')
+    todosLeft.textContent = `You have ${getThingsToDo.length} todos left`
+    document.querySelector('#todos').appendChild(todosLeft)
+
+    // Add a p for each todo above (use text value)
+    filteredToDos.forEach(function(todo) {
+        const newToDo = document.createElement('p')
+        newToDo.textContent = todo.text
+        document.querySelector('#todos').appendChild(newToDo)
+    })
+}
+
+renderTodos(todos, filters)
+
+
+// Listen for new todo creation when button is clicked
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    // e.target.textContent = 'Adding new todo...'
+    console.log('Adding new todo...')
 })
 
-// You have 2 todos left (p element)
-const todosLeft = document.createElement('h2')
-todosLeft.textContent = `You have ${getThingsToDo.length} todos left`
-document.querySelector('body').appendChild(todosLeft)
-
-// Add a p for each todo above (use text value)
-toDo.forEach(function(todo) {
-    const newToDo = document.createElement('p')
-    newToDo.textContent = todo.text
-    document.querySelector('body').appendChild(newToDo)
+// New listener for todo input field text change
+document.querySelector('#new-todo-text').addEventListener('input', function(e) {
+    console.log(e.target.value)
 })
 
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
 
 // Remove all paragraphs with 'the' in them
 // const paragraphs = document.querySelectorAll('p')
